20e2f67fd3bc41916726f0dd7d8e6b20f34dc843
tailan <tailansanttos02@gmail.com>
2025-07-14 22:07:28 -0300

    feat: first commit

---
+++ src/main/java/com/tailan/controle_de_despesas/ControleDeDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.tailan.controle_de_despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeDespesasApplication.class, args);
+	}
+
+}
