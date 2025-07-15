02b5973cf8aa630e67f2cb4671fd821b4c681a4d
tailan <tailansanttos02@gmail.com>
2025-07-14 22:20:27 -0300

    feat: enum de roles criado,  e implementado o Userdetails na entidade User.

--- src/main/java/com/tailan/controle_de_despesas/entities/user/User.java
+++ src/main/java/com/tailan/controle_de_despesas/entities/user/User.java
@@ -1,14 +1,19 @@
-package com.tailan.controle_de_despesas.entities;
+package com.tailan.controle_de_despesas.entities.user;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
@@ -18,6 +23,9 @@ public class User {
     private String email;
     private String password;
     private LocalDateTime creationDate;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 
     public User() {
     }
@@ -63,9 +71,36 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
     public String getPassword() {
         return password;
     }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;    }
 
     public void setPassword(String password) {
         this.password = password;
