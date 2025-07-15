5abc86aeb074592155e7c592684e967817444bac
tailan <tailansanttos02@gmail.com>
2025-07-15 18:51:05 -0300

    feat: implementado o serviço para carregar os detalhes do usuario quando tentar autenticar.

---
+++ src/main/java/com/tailan/controle_de_despesas/service/user/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.tailan.controle_de_despesas.service.user;
+
+import com.tailan.controle_de_despesas.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Usúario não encontrado."));
+    }
+}
