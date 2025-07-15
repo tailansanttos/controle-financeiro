03a60a04d2f0d5c45e3bdf48a1a325ac3aab1b55
tailan <tailansanttos02@gmail.com>
2025-07-14 23:10:15 -0300

    feat: operação de registrar usuario implementado.

---
+++ src/main/java/com/tailan/controle_de_despesas/service/user/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.tailan.controle_de_despesas.service.user;
+
+import com.tailan.controle_de_despesas.config.JwtService;
+import com.tailan.controle_de_despesas.dto.user.LoginRequestDto;
+import com.tailan.controle_de_despesas.dto.user.LoginResponseDto;
+import com.tailan.controle_de_despesas.dto.user.UserRequestDto;
+import com.tailan.controle_de_despesas.dto.user.UserResponseDto;
+import com.tailan.controle_de_despesas.entities.user.User;
+import com.tailan.controle_de_despesas.entities.user.UserRole;
+import com.tailan.controle_de_despesas.mapper.usuario.UserMapper;
+import com.tailan.controle_de_despesas.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final JwtService tokenService;
+    private final AuthenticationManager authenticationManager;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, JwtService tokenService, AuthenticationManager authenticationManager, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.tokenService = tokenService;
+        this.authenticationManager = authenticationManager;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public UserResponseDto register(UserRequestDto userRequestDto) {
+        String email = userRequestDto.email();
+
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent()) {
+            throw new RuntimeException("Usuario com esse email já cadastrado");
+        }
+        String password = userRequestDto.password();
+
+        String passwordEncrypte = new BCryptPasswordEncoder().encode(password);
+
+        User newUser = new User();
+        newUser.setEmail(email);
+        newUser.setPassword(passwordEncrypte);
+        newUser.setCpf(userRequestDto.cpf());
+        newUser.setName(userRequestDto.name());
+        newUser.setCreationDate(LocalDateTime.now());
+        newUser.setRole(UserRole.USER);
+
+        User savedUser = userRepository.save(newUser);
+        return userMapper.entityToDto(savedUser);
+    }
+
+    @Override
+    public LoginResponseDto login(LoginRequestDto loginRequestDto) {
+        return null;
+    }
+}
