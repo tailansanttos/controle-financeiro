371320e87f6d72fe0f3cb4df9b084f97d1fbf043
tailan <tailansanttos02@gmail.com>
2025-07-16 00:31:23 -0300

    refactor

--- src/main/java/com/tailan/controle_de_despesas/repositories/CategoryRepository.java
+++ src/main/java/com/tailan/controle_de_despesas/repositories/CategoryRepository.java
@@ -1,11 +1,19 @@
 package com.tailan.controle_de_despesas.repositories;
 
 import com.tailan.controle_de_despesas.entities.category.Category;
+import com.tailan.controle_de_despesas.entities.category.CategoryType;
+import com.tailan.controle_de_despesas.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    Optional<Category> findByIdAndUser(UUID id, User user);
+    List<Category> findByUser(User user);
+    Optional<Category> findByUserAndNameAndType(User user, String categoryName, CategoryType type);
 }
